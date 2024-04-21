1d14878985af3a155e8f2039d4a52f825a6c6a76
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2024-04-21 20:48:29 +0530

    done ✅✅

---
+++ callbackhook/src/App1.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useCallback } from "react";
+
+const App1 = () => {
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState(0);
+  const [num1] = useState(4);
+  const [num2] = useState(5);
+
+  const sum = useCallback(() => num1 + num2, [num1, num2]);
+  const buildArray = useCallback(() => [num1, num2],[num1,num2]);
+
+  useEffect(() => {
+    console.log(`New array: ${buildArray()}`);
+    setResult(buildArray());
+  }, [buildArray]);
+
+  return (
+    <main className="App">
+      <input
+        type="text"
+        placeholder="input"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      ></input>
+      <h1>Output :{userInput || "--"}</h1>
+    </main>
+  );
+};
+
+export default App1;
